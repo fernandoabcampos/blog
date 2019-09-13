abfef9f3e4754a1a72b40a4438a4a110933d692a
Fernando Barbeiro <fernandoabcampos@gmail.com>
2019-09-13 18:12:32 +0200

    JUnit4 and 5 require Before and After configuration be static (companion objects in Kotlin). With the commit configuration, we'll have instantiated the test classes one time per class instead of one per method.

--- src/test/kotlin/com/wordpress/fernandoabcampos/blog/IntegrationTests.kt
+++ src/test/kotlin/com/wordpress/fernandoabcampos/blog/IntegrationTests.kt
@@ -1,6 +1,8 @@
 package com.wordpress.fernandoabcampos.blog
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,11 +12,21 @@ import org.springframework.http.HttpStatus
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>("/", String::class.java)
         Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         Assertions.assertThat(entity.body).contains("<h1>Blog</h1>")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 
 }
